f16a88af947d5fdd71fb856132dabaebfc531917
shbh1991 <shbh1991@gmail.com>
2019-12-20 16:49:45 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,20 @@ import { DataService } from './service/data.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'sapientTest';
-  data;
+  title = 'RockedTest';
+  data; bindedData;
   constructor(private dataService: DataService) { }
   ngOnInit() {
     this.dataService.getData().subscribe((data) => {
-      this.data = data;
+      this.bindedData = data['results'];
     });
+  }
+  onDropdownChange(event) {
+    if (event.target.value == 'asc') {
+      this.bindedData.sort(((a, b) => a.id - b.id));
+    }
+    if (event.target.value == 'desc') {
+      this.bindedData.sort(((a, b) => b.id - a.id));
+    }
   }
 }
