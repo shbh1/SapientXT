30b9ec9fa2874511655889837c933d93df84e599
ShashiBhusha-n <shbh1991@gmail.com>
2019-12-20 10:44:36 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchNamePipe } from './search-name.pipe';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchNamePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
